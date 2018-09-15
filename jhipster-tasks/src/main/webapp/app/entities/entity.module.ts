ab7ea2259356ec4feee01d5b08b32565f988adff
SlawomirR <no.for.spam@wp.pl>
2018-09-15 09:55:00 +0200

    Created entity using jHipster entity sub generator

--- jhipster-tasks/src/main/webapp/app/entities/entity.module.ts
+++ jhipster-tasks/src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,13 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
+import { TasksTaskModule } from './task/task.module';
 
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        TasksTaskModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
